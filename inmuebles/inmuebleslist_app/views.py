255e62c02fe787da8cbc97d8a691e904a9506258
Jhon <jhonedin1888@hotmail.com>
2021-12-20 21:35:15 -0500

    Se crea la consulta de un inmueble por su id y que se retorne el dato en formato json

--- inmuebles/inmuebleslist_app/views.py
+++ inmuebles/inmuebleslist_app/views.py
@@ -4,9 +4,22 @@ from django.http import JsonResponse
 from inmuebleslist_app.models import Inmueble
 # Create your views here.
 
+# Obtener todos los datos de inmueble
 def inmueble_list(request):
     inmuebles = Inmueble.objects.all()
     data = {
         'inmuebles': list(inmuebles.values())
+    }
+    return JsonResponse(data)
+
+# Busqueda por Id en inmueble
+def inmueble_detalle(request,pk):
+    inmueble = Inmueble.objects.get(pk=pk)
+    data = {
+        'direccion' : inmueble.direccion,
+        'pais' : inmueble.pais,
+        'descripcion' : inmueble.descripcion,
+        'imagen' : inmueble.imagen,
+        'active' : inmueble.active,
     }
     return JsonResponse(data)
